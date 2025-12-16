9732d965fc04b31b1abd6cc47b8c55491d6fd434
mahdahar <89adham@gmail.com>
2025-12-16 15:28:43 +0700

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addWatchTarget("src/css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts"
+    }
+  };
+};
---
+++ src/about.md
@@ -0,0 +1,10 @@
+---
+layout: base.njk
+title: About Me
+---
+
+# About Me
+
+I am a developer who loves to build things.
+
+This portfolio is a reflection of my work and my passion for technology.
